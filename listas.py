e623fc2339c726689727b303372b739d31b322e5
Geovana Mirely Dias Sanches <162985900+Geovanamirely16@users.noreply.github.com>
2024-04-17 08:32:42 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,11 @@
+listaNomes = []
+
+listaNomes.append("Aylla")
+listaNomes.append("Geovana")
+listaNomes.append("Maria Aparecida")
+
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print(nome)
+    
